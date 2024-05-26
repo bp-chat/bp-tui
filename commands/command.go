e38f9624939245f6cb2dcce1b247565ecdff32b7
Pedro Santos <phpdro@gmail.com>
2024-05-25 21:03:57 -0300

    wip: add decode test

--- commands/command.go
+++ commands/command.go
@@ -13,28 +13,72 @@ type Command struct {
 	args    [][]uint8
 }
 
+const separator uint8 = 0x0a
+
 func (cmd *Command) encode() ([]uint8, error) {
 	buffer := new(bytes.Buffer)
-	err_msg := errors.New("Command could not be parsed")
 
-	if err := write(buffer, cmd.version); err != nil {
-		return nil, err_msg
+	if err := write(buffer, cmd.version, false); err != nil {
+		return nil, err
 	}
-	if err := write(buffer, 0x0a); err != nil {
-		return nil, err_msg
+
+	if err := write(buffer, cmd.sync_id, false); err != nil {
+		return nil, err
+	}
+	if err := write(buffer, cmd.id, false); err != nil {
+		return nil, err
+	}
+	for i, element := range cmd.args {
+		if err := write(buffer, element, i+1 == len(cmd.args)); err != nil {
+			return nil, err
+		}
 	}
 	return buffer.Bytes(), nil
 }
 
-func write(b *bytes.Buffer, data any) error {
-	return binary.Write(b, binary.BigEndian, data)
+func write(b *bytes.Buffer, data any, is_last bool) error {
+	var err = binary.Write(b, binary.BigEndian, data)
+	if err != nil {
+		return err
+	}
+	if is_last {
+		return nil
+	}
+	return binary.Write(b, binary.BigEndian, separator)
 }
 
-func decode(data []uint8) Command {
-	return Command{
-		0,
-		1,
-		2,
-		[][]uint8{},
+func decode(source []uint8, arg_count int) (*Command, error) {
+	if len(source) < 8 {
+		return nil, errors.New("source is to small, it must be at least 8 bytes")
 	}
+	if arg_count > 10 {
+		return nil, errors.New("It looks like to many args")
+	}
+	if source[2] != separator || source[4] != separator || source[7] != separator {
+		return nil, errors.New("Invalid data format")
+	}
+	var args [][]uint8
+	var c_arg []uint8
+	for i := 8; i < len(source); i++ {
+		elem := source[i]
+		if elem != separator {
+			c_arg = append(c_arg, elem)
+			if i < len(source)-1 {
+				continue
+			}
+		}
+		args = append(args, c_arg)
+		c_arg = make([]uint8, 0)
+	}
+
+	if len(args) != arg_count {
+		return nil, errors.New("Argument count mismatch")
+	}
+
+	return &Command{
+		uint16(source[0])<<8 | uint16(source[1]),
+		source[3],
+		uint16(source[5])<<8 | uint16(source[6]),
+		args,
+	}, nil
 }
--- commands/command_test.go
+++ commands/command_test.go
@@ -14,8 +14,44 @@ func TestEncode(t *testing.T) {
 			{0x02, 0x03},
 		},
 	}
-	data := cmd.encode()
-	if data[0] != 0xf0 {
-		t.Errorf("expected %x and got %x", 0xf0, data[0])
+	expected := []uint8{0xf0, 0x0f, 0x0a, 0x05, 0x0a, 0x0f, 0xf0, 0x0a, 0x01, 0x0a, 0x02, 0x03}
+	data, err := cmd.encode()
+	if err != nil {
+		t.Errorf("error while encoding the data")
+		t.Error(err)
+		return
+	}
+	if len(data) != len(expected) {
+		t.Fatalf("Different len between data %v and array %v", data, expected)
+	}
+	for index, element := range expected {
+
+		if data[index] != element {
+			t.Errorf("expected %x and got %x", element, data[index])
+		}
+	}
+}
+
+func TestDecode(t *testing.T) {
+	source := []uint8{0xf0, 0x0f, 0x0a, 0x05, 0x0a, 0x0f, 0xf0, 0x0a, 0x01, 0x0a, 0x02, 0x03}
+	actual, err := decode(source, 2)
+	if err != nil {
+		t.Fatalf("Could not decode data %v", err)
+	}
+	if actual.version != 0xf00f {
+		t.Errorf("Could not parse id, expected %d and got %d", 0xf00f, actual.id)
+	}
+	if actual.sync_id != 5 {
+		t.Errorf("Could not parse sync id, expected %d and got %d", 0x05, actual.sync_id)
+	}
+	if actual.id != 0x0ff0 {
+		t.Errorf("Could not parse version, expected %d and got %d", 0x0ff0, actual.version)
+	}
+	if actual.args[0][0] != 0x01 {
+		t.Errorf("Could not parse first argument, expected %d and got %d", 0x01, actual.args[0][0])
+	}
+	if actual.args[1][0] != 0x02 || actual.args[1][1] != 0x03 {
+		t.Errorf("Could not parse second argument, expected %d and got %d", 0x02, actual.args[1][0])
+		t.Errorf("Could not parse second argument, expected %d and got %d", 0x03, actual.args[1][1])
 	}
 }
